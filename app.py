e8dfb9116433faa8c89ee28391711ccba222c65c
pochinoritaro <pochimoritaro@gmail.com>
2024-02-28 17:18:17 +0900

    動作テストスクリプト(app.py)を追加

--- app.py
+++ app.py
@@ -11,6 +11,5 @@ hr = HumanResourse(
     )
 
 hr.access_token = config["freee"]["ACCESS_TOKEN"]
-hr.company_id = hr.get_users_me()["companies"][0]["id"]
-print(hr.get_groups())
-print(hr.get_employee_work_record_summary(employee_id=2363402, year=2024, month=1, work_records=True))
+me = hr.get_users_me()
+print(me)
