b7173d0f29265c5b70960d85ce211a971b38788d
pochinoritaro <pochimoritaro@gmail.com>
2024-02-29 16:10:28 +0900

    インデントをTABからスペースに修正

--- freee_sdk/utils/internal_utils.py
+++ freee_sdk/utils/internal_utils.py
@@ -1,24 +1,27 @@
 from urllib.parse import urljoin, urlencode
 
 def _remove_none_values(d: dict) -> dict:
+    print(d)
     if d is dict:
+        print(d)
+        
         return {k: v for k, v in d.items() if v is not  None}
     else:
-        return dict()
+        return {}
 
 def _get_url(base_url: str, endpoint_url: str) -> str:
-	return urljoin(base_url, endpoint_url)
-	
+    return urljoin(base_url, endpoint_url)
+    
 def _add_query(
-		base_url: str,
-		query_param: dict
-	) -> str:
-		return "{}?{}".format(base_url, urlencode(query_param))
+        base_url: str,
+        query_param: dict
+    ) -> str:
+        return "{}?{}".format(base_url, urlencode(query_param))
 
 def create_headers(access_token: dict) -> dict:
-	return {
-		"accept": "application/json",
-		"Content-Type": "application/json",
-		"Authorization": f"Bearer {access_token}",
-		"FREEE-VERSION": "2022-02-01"
-		}
+    return {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+        "FREEE-VERSION": "2022-02-01"
+        }
