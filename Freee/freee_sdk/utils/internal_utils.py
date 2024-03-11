452e2c606c9612c061decda19d7eef847c56a3bd
pochinoritaro <pochimoritaro@gmail.com>
2024-03-11 15:53:02 +0900

    #35 internal_utils.pyの_remove_none_valuesを修正 1. 辞書の値をjsonに変換する

--- Freee/freee_sdk/utils/internal_utils.py
+++ Freee/freee_sdk/utils/internal_utils.py
@@ -1,19 +1,23 @@
+from json import dumps
 from urllib.parse import urljoin, urlencode
 
 def _remove_none_values(d: dict) -> dict:
     if isinstance(d, dict):
-        return {k: v for k, v in d.items() if v is not None}
+        return {k: dumps(v) for k, v in d.items() if v is not None}
     else:
         return {}
 
+
 def _get_url(base_url: str, endpoint_url: str) -> str:
     return urljoin(base_url, endpoint_url)
-    
+
+
 def _add_query(
         base_url: str,
         query_param: dict
     ) -> str:
         return "{}?{}".format(base_url, urlencode(query_param))
+
 
 def create_headers(access_token: dict) -> dict:
     return {
